95bf06322d46315de2745a19d1f662b8d9346da4
Ruixi Fan <ruixi@typeface.ai>
2024-12-02 16:53:09 -0800

    test code

---
+++ commands.md
@@ -0,0 +1,8 @@
+
+
+### Executing DeepFloyd
+
+
+```shell
+PYTHONPATH=/Users/ruixi/workspace/IF python playground/DeepFloyd/inpaint.py
+```
--- playground/DeepFloyd/inpaint.py
+++ playground/DeepFloyd/inpaint.py
@@ -21,39 +21,40 @@ mask_image = download_image(mask_url).resize((512, 512))
 mask_image = ToTensor()(mask_image).unsqueeze(0)  # (1, 3, 512, 512)
 
 # Run locally
-device = 'cuda:5'
-cache_dir = "/comp_robot/rentianhe/weights/IF/"
-if_I = IFStageI('IF-I-L-v1.0', device=device, cache_dir=cache_dir)
-if_II = IFStageII('IF-II-L-v1.0', device=device, cache_dir=cache_dir)
-if_III = StableStageIII('stable-diffusion-x4-upscaler', device=device, cache_dir=cache_dir)
-t5 = T5Embedder(device=device, cache_dir=cache_dir)
-result = inpainting(
-    t5=t5, if_I=if_I,
-    if_II=if_II,
-    if_III=if_III,
-    support_pil_img=init_image,
-    inpainting_mask=mask_image,
-    prompt=[
-        'A Panda'
-    ],
-    seed=42,
-    if_I_kwargs={
-        "guidance_scale": 7.0,
-        "sample_timestep_respacing": "10,10,10,10,10,0,0,0,0,0",
-        'support_noise_less_qsample_steps': 0,
-    },
-    if_II_kwargs={
-        "guidance_scale": 4.0,
-        'aug_level': 0.0,
-        "sample_timestep_respacing": '100',
-    },
-    if_III_kwargs={
-        "guidance_scale": 9.0,
-        "noise_level": 20,
-        "sample_timestep_respacing": "75",
-    },
-)
-if_I.show(result['I'], 2, 3)
-if_I.show(result['II'], 2, 6)
-if_I.show(result['III'], 2, 14)
+if __name__ == "__main__":
+    device = 'mps'
+    cache_dir = "./cache_dir"
+    if_I = IFStageI('IF-I-L-v1.0', device=device, cache_dir=cache_dir)
+    if_II = IFStageII('IF-II-L-v1.0', device=device, cache_dir=cache_dir)
+    if_III = StableStageIII('stable-diffusion-x4-upscaler', device=device, cache_dir=cache_dir)
+    t5 = T5Embedder(device=device, cache_dir=cache_dir)
+    result = inpainting(
+        t5=t5, if_I=if_I,
+        if_II=if_II,
+        if_III=if_III,
+        support_pil_img=init_image,
+        inpainting_mask=mask_image,
+        prompt=[
+            'A Panda'
+        ],
+        seed=42,
+        if_I_kwargs={
+            "guidance_scale": 7.0,
+            "sample_timestep_respacing": "10,10,10,10,10,0,0,0,0,0",
+            'support_noise_less_qsample_steps': 0,
+        },
+        if_II_kwargs={
+            "guidance_scale": 4.0,
+            'aug_level': 0.0,
+            "sample_timestep_respacing": '100',
+        },
+        if_III_kwargs={
+            "guidance_scale": 9.0,
+            "noise_level": 20,
+            "sample_timestep_respacing": "75",
+        },
+    )
+    if_I.show(result['I'], 2, 3)
+    if_I.show(result['II'], 2, 6)
+    if_I.show(result['III'], 2, 14)
 
